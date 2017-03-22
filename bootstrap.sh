5ae1cc7d36e1d489c7919854512d852a2e9d6dbc
Saad Siddiqui <saad@sids92.co>
2017-03-21 21:57:42 -0500

    done working on this for class

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# update all existing packages
+sudo yum -y update
+# install wget and dos2unix
+sudo yum -y install wget
+sudo yum -y install dos2unix
+# download java
+sudo wget --no-cookies \
+  --no-check-certificate \
+  --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+  "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm" \
+  -O /tmp/jdk-8-linux-x64.rpm
+# install java
+sudo yum -y install /tmp/jdk-8-linux-x64.rpm
+sudo rm /tmp/jdk-8-linux-x64.rpm
